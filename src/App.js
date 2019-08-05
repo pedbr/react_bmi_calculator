4a6019e8a798507bf57fefdb04e937a5c2d24ad3
pedro_bras <pedro.c.bras@gmail.com>
2019-08-05 10:35:40 +0200

    adds weight and height input boxes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      
+      <div>
+        <label>Weight(kg)</label>
+        <input name="weight" />
+      </div>
+
+      <div>
+        <label>Height(cm)</label>
+        <input name="height" />
+      </div>
     </div>
   );
 }
