9a0d97111f1bf711fd47a7bfb7f7cccf98994cb9
pedro_bras <pedro.c.bras@gmail.com>
2019-08-05 10:59:57 +0200

    declares weight and height variables on display result component and imports it to app js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import DisplayResult from './Components/displayResult';
 import "./App.css";
 
 class App extends Component {
--- src/Components/displayResult.js
+++ src/Components/displayResult.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class DisplayResult extends Component {
+  calculate() {
+    let weight = this.props.weight;
+    let height = this.props.height;
+  }
 
   render() {
-    return (
-      <div id='response'>
-      </div>
-    )
+    return <div id="response">{this.calculate()}</div>;
   }
 }
 
-export default DisplayResult
+export default DisplayResult;
