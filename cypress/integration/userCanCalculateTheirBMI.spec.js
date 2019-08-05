340b0d4f9a7b5c2654b47951452c134211aed4d0
pedro_bras <pedro.c.bras@gmail.com>
2019-08-05 14:59:01 +0200

    fixes test file removing imperial mode

--- cypress/integration/userCanCalculateTheirBMI.spec.js
+++ cypress/integration/userCanCalculateTheirBMI.spec.js
@@ -1,52 +1,34 @@
-describe('BMI Converter', () => {
-    before(function() {
-        cy.visit('http://localhost:3000');
+describe("BMI Converter", () => {
+  before(function() {
+    cy.visit("http://localhost:3000");
+  });
+
+  beforeEach(function() {
+    // Reload the application between tests to reset state
+    cy.reload();
+  });
+
+  it('should display "BMI Converter" text on page', () => {
+    cy.contains("BMI Calculator");
+  });
+
+  it('should display "Result:" text on page', () => {
+    cy.contains("Result:");
+  });
+
+  describe("Metric method", () => {
+    beforeEach(() => {
+      // This before block will be executed prior to each test in this describe block
+      cy.get('input[name="weight"]').type("95");
+      cy.get('input[name="height"]').type("186");
     });
 
-    beforeEach(function() {
-        // Reload the application between tests to reset state
-        cy.reload();
+    it("displays assessment", async () => {
+      cy.contains("You are Overweight");
     });
 
-    it('should display "BMI Converter" text on page', () => {
-        cy.contains('BMI Converter');
+    it("displays BMI value", async () => {
+      cy.contains("BMI of 27.46");
     });
-
-    it('should display "Result:" text on page', () => {
-        cy.contains('Result:');
-    });
-
-    describe('Metric method', () => {
-        beforeEach(() => {
-            // This before block will be executed prior to each test in this describe block
-            cy.get('select[id="method"]').select('metric')
-            cy.get('input[name="weight"]').type('95')
-            cy.get('input[name="height"]').type('186')
-        })
-
-        it('displays assesment', async () => {   
-            cy.contains('You are Overweight')
-        })
-
-        it('displays BMI value', async () => {   
-            cy.contains('BMI of 27.46')
-        })
-    })
-
-    describe('Imperial method', async () => {
-        beforeEach( async () => {
-            // This before block will be executed prior to each test in this describe block
-            cy.get('select[id="method"]').select('imperial')
-            cy.get('input[name="weight"]').type('200')
-            cy.get('input[name="height"]').type('73')
-        })
-
-        it('displays assesment', async () => {   
-            cy.contains('You are Overweight')
-        })
-
-        it('displays BMI value', async () => {   
-            cy.contains('BMI of 26.38')
-        })
-    })
-}); 
+  });
+});
