2f12bcbc5099950c41d09d7d6e31c7e5429f2950
pedro_bras <pedro.c.bras@gmail.com>
2019-08-05 10:47:10 +0200

    changes from function to class to add custructor and adds state to the component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <div>
-        <label>Weight(kg)</label>
-        <input name="weight" />
-      </div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weight: "",
+      height: ""
+    };
+  }
 
+  render() {
+    return (
       <div>
-        <label>Height(cm)</label>
-        <input name="height" />
+        <div>
+          <label>Weight(kg)</label>
+          <input
+            name="weight"
+            value={this.state.weight}
+            onChange={e => this.setState({ weight: e.target.value })}
+          />
+        </div>
+
+        <div>
+          <label>Height(cm)</label>
+          <input
+            name="height"
+            value={this.state.height}
+            onChange={e => this.setState({ height: e.target.value })}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
