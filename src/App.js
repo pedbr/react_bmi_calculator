3ea6404729de1910bb86c72a65d5ffca0ad8f09a
pedro_bras <pedro.c.bras@gmail.com>
2019-08-05 11:31:13 +0200

    adds title to main page and flex box to css

--- src/App.js
+++ src/App.js
@@ -13,14 +13,15 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <div>
-          <label>Weight(kg)</label>
+      <div className="main-div">
+        <div className="title">BMI Calculator</div>
+        <div className="weight-div">
+          <label>Weight (kg)</label>
           <input name="weight" value={this.state.weight} onChange={ (e) => this.setState({ weight: e.target.value })} />
         </div>
 
-        <div>
-          <label>Height(cm)</label>
+        <div className="height-div">
+          <label>Height (cm)</label>
           <input name="height" value={this.state.height} onChange={ (e) => this.setState({ height: e.target.value })} />
         </div>
 
