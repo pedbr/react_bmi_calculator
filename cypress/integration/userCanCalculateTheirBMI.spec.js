3e0ba1b217bd14e4bd901ed735fcc37703730afb
pedro_bras <pedro.c.bras@gmail.com>
2019-08-06 11:45:07 +0200

    adds testing including imperial system

--- cypress/integration/userCanCalculateTheirBMI.spec.js
+++ cypress/integration/userCanCalculateTheirBMI.spec.js
@@ -1,34 +1,48 @@
-describe("BMI Converter", () => {
+describe('BMI Converter', () => {
   before(function() {
-    cy.visit("http://localhost:3000");
+      cy.visit('http://localhost:3000');
   });
 
   beforeEach(function() {
-    // Reload the application between tests to reset state
-    cy.reload();
+      // Reload the application between tests to reset state
+      cy.reload();
   });
 
   it('should display "BMI Converter" text on page', () => {
-    cy.contains("BMI Calculator");
+      cy.contains('BMI Converter');
   });
 
-  it('should display "Result:" text on page', () => {
-    cy.contains("Result:");
-  });
+  describe('Metric method', () => {
+      beforeEach(() => {
+          // This before block will be executed prior to each test in this describe block
+          cy.get('select[id="method"]').select('metric')
+          cy.get('input[name="weight"]').type('95')
+          cy.get('input[name="height"]').type('186')
+      })
 
-  describe("Metric method", () => {
-    beforeEach(() => {
-      // This before block will be executed prior to each test in this describe block
-      cy.get('input[name="weight"]').type("95");
-      cy.get('input[name="height"]').type("186");
-    });
+      it('displays assesment', async () => {   
+          cy.contains('You are Overweight')
+      })
 
-    it("displays assessment", async () => {
-      cy.contains("You are Overweight");
-    });
+      it('displays BMI value', async () => {   
+          cy.contains('BMI of 27.46')
+      })
+  })
 
-    it("displays BMI value", async () => {
-      cy.contains("BMI of 27.46");
-    });
-  });
-});
+  describe('Imperial method', async () => {
+      beforeEach( async () => {
+          // This before block will be executed prior to each test in this describe block
+          cy.get('select[id="method"]').select('imperial')
+          cy.get('input[name="weight"]').type('200')
+          cy.get('input[name="height"]').type('73')
+      })
+
+      it('displays assessment', async () => {   
+          cy.contains('You are Overweight')
+      })
+
+      it('displays BMI value', async () => {   
+          cy.contains('BMI of 26.38')
+      })
+  })
+}); 
