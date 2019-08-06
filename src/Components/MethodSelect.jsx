6dcd5e2901bb96b69bafa9d3f6146d40c2a0848a
pedro_bras <pedro.c.bras@gmail.com>
2019-08-06 14:29:59 +0200

    adds styling to the method selector

--- src/Components/MethodSelect.jsx
+++ src/Components/MethodSelect.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "../App.css";
 
 function MethodSelect(props) {
   return (
     <>
       <div>
       <label>Method</label>
-        <select name="method" id="method" onChange={props.onChangeValue} value={props.method}>
+        <select className="method-selector" name="method" id="method" onChange={props.onChangeValue} value={props.method}>
           <option value="metric">Metric</option>
           <option value="imperial">Imperial</option>
         </select>
